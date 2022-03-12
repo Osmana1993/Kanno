6d90b07ec23acc0f0a1950f29b685d9d0ac0559d
osman <othmanalialamin@gmail.com>
2022-03-12 11:36:15 -0800

    third

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+})
+/*document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = "red";
+
+});*/
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "0px";
+
+});
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
